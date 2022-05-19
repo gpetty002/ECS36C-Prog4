1f9a26f7f6abc1fb6b3ba76ca696e71c80ddb3fc
gissellepetty@gmail.com <gissellepetty@gmail.com>
2022-05-19 13:52:44 -0700

    added Output test case

--- test_bstream.cc
+++ test_bstream.cc
@@ -41,6 +41,30 @@ TEST(BStream, input) {
   std::remove(filename.c_str());
 }
 
+TEST(BStream, Output) {
+  std::string filename{"test_bstream_output"};
+
+  // const unsigned char val[] = {
+  //   0x58, 0x90, 0xab, 0x08,
+  //   0x00, 0x4e, 0xdb, 0x40,
+  // };
+  // Equivalent in binary is:
+  // 0101100010010000101010110000100000000000010011101101101101000000
+  // ^5  ^8  ^9  ^0  ^a  ^b  ^0  ^8  ^0  ^0  ^4  ^e  ^d  ^b  ^4  ^0
+
+  // Write this to a file
+  std::ofstream ofs(filename, std::ios::out |
+                    std::ios::trunc);
+  BinaryOutputStream bos(ofs);
+  bos.PutBit(0);
+  bos.PutBit(1);
+  bos.PutChar(0x62);
+  bos.PutChar(0x42);
+  bos.PutBit(1);
+  bos.PutInt(0x58400276);
+  bos.PutChar(0xDA);
+}
+
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
